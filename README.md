16d1be4ad15c737ca9e9ef7f7d7943c790e8553f
Franccesco Antonio <franccesco_@hotmail.com>
2020-02-14 17:47:55 -0300

    Updating Files
    Finally I finished my back-end application, with nodemailer to send emails, sentry to watch errors. Now it's time to start the fron-end with react.
    OS: Arch Linux

--- README.md
+++
@@ -1,2 +0,0 @@
-# RocketSeat Bootcamp GoStack 8.0
-# Este é o meu repositório do curso Bootcamp distribuido pela Rocketseat
--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -5,6 +5,8 @@ import User from '../models/user';
 import File from '../models/File';
 import Appointment from '../models/Appointments';
 import Notifications from '../schemas/Notifications';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -13,7 +15,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -112,9 +114,22 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
-    if (appointment.user.id !== req.userId) {
+    if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment",
       });
@@ -131,6 +146,10 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
 
     return res.json(appointment);
   }
